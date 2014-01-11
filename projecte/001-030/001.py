49c8011718699acd886db792aa09ea0ea9a8934b
hogejiro <combatechizen2@gmail.com>
2014-01-11 13:23:12 +0900

    modify format

--- projecte/001-030/001.py
+++ projecte/001-030/001.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Multiples of 3 and 5
+ref: http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%201
+"""
+
 list = range(1, 1000)
 list_filtered = [l for l in list if l % 3 == 0 or l % 5 == 0]
-print sum(list_filtered) # 233168
+print sum(list_filtered)
+# 233168
