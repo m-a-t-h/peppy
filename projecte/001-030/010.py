f73610e427e33c633f830155fdc08aeead808b7c
hogejiro <combatechizen2@gmail.com>
2014-01-13 14:49:02 +0900

    solve 010

---
+++ projecte/001-030/010.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Summation of primes
+ref: http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%2010
+"""
+
+import sys,os
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../module/'))
+from prime import Prime
+
+def main():
+    P = Prime()
+    print sum(P.get_primes_by_limit_number(2000000))
+
+if __name__ == '__main__':
+    main()
+
+# 142913828922
